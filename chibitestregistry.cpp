b2aef17a15d17aa49dd803a5b474e8b6b26b43c2
Sean Cross <xobs@kosagi.com>
2016-06-30 17:44:05 +0800

    chibitestregistry: add a registry for finding modules
    In the future, modules will be loaded as .so files.  The registry is
    the first step towards that.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

---
+++ chibitestregistry.cpp
@@ -0,0 +1,48 @@
+#include "chibitest.h"
+#include "chibitestregistry.h"
+
+static void ct_test_message(void *testObj, TestMessageType messageType,
+                            int value, const char *message) {
+    static_cast<ChibiTest*>(testObj)->testData(
+        static_cast<ChibiTest*>(testObj)->testName(),
+        messageType,
+        value,
+        message);
+}
+
+static void ct_msleep(void *testObj, int msecs) {
+    static_cast<ChibiTest*>(testObj)->msleep(msecs);
+}
+
+static void ct_set_gpio(void *testObj, int gpio, int val) {
+    static_cast<ChibiTest*>(testObj)->setGpio(gpio, val);
+}
+
+int  ct_get_gpio(void *testObj, int gpio) {
+    return static_cast<ChibiTest*>(testObj)->getGpio(gpio);
+}
+
+void ct_unexport_gpio(void *testObj, int gpio) {
+    static_cast<ChibiTest*>(testObj)->unexportGpio(gpio);
+}
+
+static const FrameworkCallbacks frameworkCallbacks = {
+  /* test_message */  ct_test_message,
+  /* msleep */        ct_msleep,
+  /* set_gpio */      ct_set_gpio,
+  /* get_gpio */      ct_get_gpio,
+  /* unexport_gpio */ ct_unexport_gpio,
+};
+
+
+ChibiTestRegistry::ChibiTestRegistry() {
+}
+
+void ChibiTestRegistry::addModule(const struct test_module *module) {
+    module->module_init(&frameworkCallbacks);
+    registry.insert(module->name, QVariant::fromValue((void *)module));
+}
+
+const TestModule *ChibiTestRegistry::getModule(const QString &name) {
+    return (const TestModule *)registry.value(name).value<void *>();
+}
---
+++ chibitestregistry.h
@@ -0,0 +1,22 @@
+#ifndef CHIBITESTREGISTRY_H
+#define CHIBITESTREGISTRY_H
+
+#include <QObject>
+#include <QHash>
+#include <QVariant>
+#include "testmodule.h"
+
+class ChibiTestRegistry : public QObject
+{
+    Q_OBJECT
+
+private:
+    QHash<QString, QVariant> registry;
+
+public:
+    ChibiTestRegistry();
+    void addModule(const struct test_module *module);
+    const TestModule *getModule(const QString &name);
+};
+
+#endif // CHIBITESTREGISTRY_H
