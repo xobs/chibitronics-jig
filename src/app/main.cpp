2f3dd8492b6f021ebcb455a60b975af82cc384ef
Sean Cross <sean@xobs.io>
2016-07-22 15:22:27 +0800

    main: hide the cursor
    This is important now that the program is going fullscreen again.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,32 +1,19 @@
-//#include <QApplication>
-//#include <QGraphicsObject>
-//#include "qtquick1applicationviewer.h"
-#include <QGuiApplication>
+#include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlProperty>
 #include <QVariant>
 #include "chibisequence.h"
 
-#include <QDebug>
-
 int main(int argc, char *argv[])
 {
-    //QApplication app(argc, argv);
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
+    //QGuiApplication app(argc, argv);
 
-    //app.setOverrideCursor(Qt::BlankCursor);
+    app.setOverrideCursor(Qt::BlankCursor);
 
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    //engine.load(QUrl(QStringLiteral("src/app/main.qml")));
-
-    /*
-    viewer.addImportPath(QLatin1String("modules"));
-    viewer.setOrientation(QtQuick1ApplicationViewer::ScreenOrientationAuto);
-    viewer.setMainQmlFile(QLatin1String("qml/chibitronics-jig/main.qml"));
-    viewer.showExpanded();
-    */
 
     QObject *rootObject = engine.rootObjects().first();
     QVariant tests = QQmlProperty::read(rootObject, "tests");
