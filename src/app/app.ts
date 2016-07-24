2de58e76db2fe932f858b25bdbb2d544ee1ec183
Sean Cross <xobs@kosagi.com>
2016-07-24 20:18:10 +0800

    translation: add basic translation strings
    Used for doing internationalization of the app.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

---
+++ src/app/app.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh">
+<context>
+    <name>TestWindow</name>
+    <message>
+        <location filename="TestWindow.qml" line="89"/>
+        <source>Click to begin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="TestWindow.qml" line="211"/>
+        <source>Pass</source>
+        <translatorcomment>nar</translatorcomment>
+        <translation type="unfinished">foo</translation>
+    </message>
+    <message>
+        <location filename="TestWindow.qml" line="220"/>
+        <source>Fail</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="9"/>
+        <source>Chibitronics Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
