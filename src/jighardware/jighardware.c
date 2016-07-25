529da1631494d64cf80065c1d8f543e683e4822d
Sean Cross <xobs@kosagi.com>
2016-07-25 12:11:12 +0800

    jighardware: check gpio 16 for board mode
    Check this particular pin to determine whether it's an LtC board or
    a dataviewer board.
    
    Set global variables as necessary.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- src/jighardware/jighardware.c
+++ src/jighardware/jighardware.c
@@ -3,6 +3,9 @@
 #include <wiringPi.h>
 #include <stddef.h>
 
+#define START_BUTTON_GPIO 26
+#define MODE_SELECT_GPIO 16
+
 static const FrameworkCallbacksC *mod_callbacks;
 
 static void sendStart(void) {
@@ -14,9 +17,24 @@ static void JigHardware__init(const FrameworkCallbacksC *callbacks) {
 
     // Note: we've hacked wiringPi.c to work when not run as root.
     wiringPiSetupGpio();
-    pinMode(26, INPUT);
-    pullUpDnControl(26, PUD_UP);
-    wiringPiISR(26, INT_EDGE_FALLING, sendStart);
+
+    // Set the start button up as a GPIO input
+    pinMode(START_BUTTON_GPIO, INPUT);
+    pullUpDnControl(START_BUTTON_GPIO, PUD_UP);
+    wiringPiISR(START_BUTTON_GPIO, INT_EDGE_FALLING, sendStart);
+
+    // We use GPIO 16 (SPI CE2, header pin 36) to differentiate between the
+    // LtC and Dataviewer jigs.
+    // The Dataviewer pulls this to GND, while the LtC board leaves it floating.
+    pinMode(MODE_SELECT_GPIO, INPUT);
+    pullUpDnControl(MODE_SELECT_GPIO, PUD_UP);
+
+    if (! digitalRead(MODE_SELECT_GPIO)) {
+        mod_callbacks->send_message(NULL, SetGlobal, "devicetype", "dataviewer");
+    }
+    else {
+]        mod_callbacks->send_message(NULL, SetGlobal, "devicetype", "ltc");
+    }
 }
 
 struct test_module_c Q_DECL_EXPORT test_module = {
